887d1e36f277adf0e89a5f8cc0f1467f17ea9e17
Ignacio Tampe <Igtampe@outlook.com>
2021-01-11 12:17:03 -0400

    Add project files.

---
+++ BasicRenderTetris/Board.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Igtampe.BasicRender;
+
+namespace Igtampe.Tetris {
+    class Board {
+
+        int score;
+        int linesCleared;
+
+        /// <summary>16x10 array of booleans that will hold the playing field</summary>
+        public bool[,] BoardArray { get; private set; }
+
+        /// <summary>Creates the board</summary>
+        public Board() { 
+            BoardArray= new bool[16,10];
+            ResetBoard();
+        }
+
+        /// <summary>Resets the board. Everything is now false</summary>
+        public void ResetBoard() {for(int i = 0; i < 16; i++) {for(int j = 0; j < 10; j++) {BoardArray[i,j] = false;}}}
+
+        /// <summary>Draws the board </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public void Render(int x,int y) { 
+            
+            //Tick the board
+
+
+        }
+
+
+
+
+    }
+}
---
+++ BasicRenderTetris/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Igtampe.Tetris {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+
+
+
+            //10x16
+
+
+        }
+    }
+}
